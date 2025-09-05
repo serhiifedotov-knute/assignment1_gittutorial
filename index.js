db55452cff18b92fb671d9527082f316998d2167
SF Knute <s.fedotov@knute.edu.ua>
2025-09-05 09:58:31 +0300

    added other math functions

--- index.js
+++ index.js
@@ -5,4 +5,19 @@ function AddTwoNumber(a,b){
     return a  + b;
 }
 
+function MinusTwoNumber(a,b){
+    return a  - b;
+}
+
+
+function MultiplyTwoNumber(a,b){
+    return a  * b;
+}
+
+
+function DivideTwoNumber(a,b){
+    return a / b;
+}
+
+
 console.log(AddTwoNumber(5,20));
